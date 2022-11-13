fd10f3c8541aba3d32b450611748caa73cfcc26e
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2022-11-13 19:58:18 +0100

    Store and display added new todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
+  // [new Todo("Learn React"), new Todo("Learn TypeScript")];
 
   const addTodoHandler = (todoText: string) => {
-    console.log(todoText);
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => {
+      return [...prevTodos, newTodo];
+      // return prevTodos.concat(newTodo);
+    });
   };
 
   return (
